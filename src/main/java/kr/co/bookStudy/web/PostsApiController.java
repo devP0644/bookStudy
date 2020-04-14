7e3e44da4de56091d7eda582bbc2bdb19bccb874
devP0644 <phg0644@naver.com>
2020-04-14 10:04:03 +0900

    create frontEnd

--- src/main/java/kr/co/bookStudy/web/PostsApiController.java
+++ src/main/java/kr/co/bookStudy/web/PostsApiController.java
@@ -14,6 +14,11 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id,
                        @RequestBody PostsUpdateRequestDto requestDto) {
@@ -23,6 +28,13 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+
+        return id;
     }
 
 }
--- src/main/java/kr/co/bookStudy/web/posts/PostsService.java
+++ src/main/java/kr/co/bookStudy/web/posts/PostsService.java
@@ -2,12 +2,16 @@ package kr.co.bookStudy.web.posts;
 
 import kr.co.bookStudy.web.domain.posts.Posts;
 import kr.co.bookStudy.web.domain.posts.PostsRepository;
+import kr.co.bookStudy.web.dto.PostsListResponseDto;
 import kr.co.bookStudy.web.dto.PostsResponseDto;
 import kr.co.bookStudy.web.dto.PostsSaveRequestDto;
 import kr.co.bookStudy.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,4 +39,20 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = " +id));
+
+        postsRepository.delete(posts);
+    }
 }
+
