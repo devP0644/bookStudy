eb6d44d808d6b797c961e0a58dfd86ae6013fab2
devP0644 <phg0644@naver.com>
2020-04-14 14:58:22 +0900

    create googleLogin

---
+++ src/main/java/kr/co/bookStudy/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package kr.co.bookStudy.config.auth.dto;
+
+import kr.co.bookStudy.web.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
--- src/main/java/kr/co/bookStudy/web/IndexController.java
+++ src/main/java/kr/co/bookStudy/web/IndexController.java
@@ -1,5 +1,6 @@
 package kr.co.bookStudy.web;
 
+import kr.co.bookStudy.config.auth.dto.SessionUser;
 import kr.co.bookStudy.web.dto.PostsResponseDto;
 import kr.co.bookStudy.web.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -8,17 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
 
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
